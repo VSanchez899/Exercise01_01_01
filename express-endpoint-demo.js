3c160719a1ac0a4992c171f8236d23562e64636e
Vincent Sanchez <VSanchez899@west-mec.org>
2019-01-15 14:25:18 -0800

    added more

---
+++ express-endpoint-demo.js
@@ -0,0 +1,27 @@
+//Author: Vincent Sanchez
+//File name: 
+//Title: 
+//Exercise/Project Number:
+
+var express = require('express');
+
+var app = express();
+var fs = require('fs');
+//this is what port the server will be hosted on
+var port = 8080;
+
+app.use('/message', function name(req, res) {
+    //this tell the console that the user has requested an end point
+    console.log("user requested an endpoint!")    
+    res.send("<h2>Hello from the server endpoint!</h2>");
+});
+
+app.use('/users', function(req, res){
+    fs.readFile('./data2.json', 'utf-8', function(err, data){
+        res.send(data);
+    });
+})
+
+app.listen(port, function(){
+    console.log("Server listening on local host:%s", port)
+})
--- express-static-demo.js
+++ express-static-demo.js
@@ -9,7 +9,9 @@ var app = express();
 
 // this is the specific prot for it to be hosted on
 var port = 8080;
+//
+app.use(express.static(__dirname));
 // this listens for the port
-app.listen(port, function name() {
+app.listen(port, function() {
    console.log("Server listening on local host:%s", port);
 });
